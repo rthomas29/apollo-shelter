a685bfe048409dc6e83cee9f72f90f974856cb6c
Rakeem <thomas.rakeem.29@gmail.com>
2018-12-25 11:28:40 -0500

    Tests - db operations

--- server/src/db/crud.js
+++ server/src/db/crud.js
@@ -2,37 +2,34 @@ import Animal from '../types/animal/animal.model'
 import User from '../types/user/user.model'
 
 const db = {
-  createAnimal: animal => {
-    return Animal.create(animal)
-  },
+  createAnimal: animal => Animal.create(animal),
+
   findAllAnimals: () =>
     Animal.find({})
       .lean()
       .exec(),
-  findAnimalByField: (field, value) => {
-    return Animal.find({ [field]: value })
-      .lean()
-      .exec()
-  },
-  findAnimalById: id => {
-    return Animal.findById(id)
-      .lean()
-      .exec()
-  },
 
-  findAnimalByIdAndUpdate: (id, updatedAnimal) => {
-    return Animal.findByIdAndUpdate(id, updatedAnimal, { new: true })
+  findAnimalByField: (field, value) =>
+    Animal.find({ [field]: value })
       .lean()
-      .exec()
-  },
-  deleteAnimalById: id => {
-    return Animal.findByIdAndDelete(id)
+      .exec(),
+
+  findAnimalById: id =>
+    Animal.findById(id)
       .lean()
-      .exec()
-  },
-  createUser: user => {
-    return User.create(user)
-  }
+      .exec(),
+
+  findAnimalByIdAndUpdate: (id, updatedAnimal) =>
+    Animal.findByIdAndUpdate(id, updatedAnimal, { new: true })
+      .lean()
+      .exec(),
+
+  deleteAnimalById: id =>
+    Animal.findByIdAndDelete(id)
+      .lean()
+      .exec(),
+
+  createUser: user => User.create(user)
 }
 
 export default db
