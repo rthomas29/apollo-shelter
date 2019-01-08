990ba432c73e1facb82135c97aa938e9b6a43cd9
Rakeem <thomas.rakeem.29@gmail.com>
2019-01-07 22:25:07 -0500

    Wire client to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,54 @@
-import React, { Component } from 'react';
-import { ApolloClient } from 'apollo-boost';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from 'react-apollo';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { ApolloClient } from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloProvider, Query } from 'react-apollo'
+import './App.css'
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache()
 
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8080'
+})
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/',
-  link: new HttpLink(),
+  link: httpLink,
   cache
-});
+})
+
+const GET_ANIMALS = gql`
+  {
+    animals {
+      name
+      color
+      weight
+    }
+  }
+`
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-              Learn React
-            </a>
-          </header>
+          <Query query={GET_ANIMALS}>
+            {({ loading, error, data }) => {
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
+              return (
+                <select name="dog">
+                  {data.animals.map(animal => (
+                    <option key={animal.createdAt} value={animal.name}>
+                      {animal.name}
+                    </option>
+                  ))}
+                </select>
+              )
+            }}
+          </Query>
         </div>
       </ApolloProvider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
