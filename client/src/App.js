abefedd0c22712dd9be979359a506ffd9b4cb4de
Rakeem <thomas.rakeem.29@gmail.com>
2019-01-07 21:49:47 -0500

    Basic ApolloClient config

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
+import { ApolloClient } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
 
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/',
+  link: new HttpLink(),
+  cache
+});
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ApolloProvider>
     );
   }
 }
